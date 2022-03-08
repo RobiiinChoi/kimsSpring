d77da246572bbe5a62133647e37a37e44df315a0
RobiiinChoi <robin.choi11@gmail.com>
2022-03-08 14:47:18 +0900

    ADD JPQL vs Querydsl

---
+++ querydsl/src/test/java/com/study/querydsl/entity/QuerydslBasicTest.java
@@ -0,0 +1,84 @@
+package com.study.querydsl.entity;
+
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void testEntity() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // 초기화
+        em.flush();
+        em.clear();
+
+        // 확인
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println(member);
+            System.out.println(member.getTeam());
+        }
+    }
+
+    @Test
+    public void startJPQL() {
+        // member1을 찾기
+        Member findByJPQL = em.createQuery(
+                        "select m from Member m " +
+                                "where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findByJPQL.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        QMember m = new QMember("m"); // Qmember에 대한 변수명
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1")) // 파라미터 바인딩 처리
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
